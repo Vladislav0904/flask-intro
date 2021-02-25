c3db4af5ff0c46ff61e44231ecce3ab45458894e
Vladislav <vladislav0904@gmail.com>
2021-02-25 13:22:18 +0500

    Classwork Problem 4

--- base.py
+++ base.py
@@ -37,5 +37,41 @@ def image_mars():
 '''
 
 
+@app.route('/promotion_image')
+def bootstrap():
+    return f'''<!doctype html>
+                <html lang="ru">
+                  <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                    <link rel="stylesheet" href="static/css/style.css">
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                    <title>Колонизация</title>
+                  </head>
+                  <body>
+                  <h1><p>Жди нас, Марс!</p> </h1>
+                  <img src='{url_for('static', filename='img/mars_planet.png')}'>
+                    <div class="alert alert-primary" role="alert">
+                        Человечество вырастает из детства.
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                        Человечеству мала одна планета.
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                        Мы сделаем обитаемыми безжизненные пока планеты.
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                        И начнем с Марса!
+                    </div>
+                    <div class="alert alert-warning" role="alert">
+                        Присоединяйся!
+                    </div>
+                  </body>
+                </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
